0e95363e824828bb435ef4d77e360f0911980d0f
every_one_callme_Pie <thananonza123@gmail.com>
2025-02-14 15:41:41 +0700

    edit mymain2

---
+++ ThongJavaInClass/Week09/mypack/mymain2.java
@@ -0,0 +1,31 @@
+package Week09.mypack;
+
+class ironman {
+
+    public ironman() {
+        System.out.println("- Tony Stark");
+    }
+}
+
+class captainAmerica{
+
+    public captainAmerica() {
+        System.out.println("- Steve Roger");
+    }
+}
+
+class Avenger {
+
+    public void AvengerInitiative() {
+        ironman hero1 = new ironman();
+        captainAmerica hero2 = new captainAmerica();
+    }
+    
+}
+
+public class mymain2 {
+    public static void main(String[] args) {
+        Avenger NickFury = new Avenger();
+        NickFury.AvengerInitiative();
+    }
+}
